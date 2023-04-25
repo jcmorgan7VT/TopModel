bbfa8053c6304002a0fcb21a382498fbe5b9eb49
John <jcmorgan7@t.edu>
2023-04-24 20:43:57 -0400

    actually getting it implemented

--- dynatopTest_10_26_22.R
+++ dynatopTest_10_26_22.R
@@ -3,9 +3,11 @@
 # https://cran.r-project.org/web/packages/dynatopGIS/vignettes/dynatopGIS.html 
 
 #install.packages("dynatopGIS")
+# Dynatop implementation 4/24/23
+####################################
 library(pacman)
 
-p_load(dynatopGIS, tidyverse, raster, rgdal, sp, mapview,
+p_load(dynatop, tidyverse, raster, rgdal, sp, mapview,
        rgeos, spdplyr, whitebox)
 #working from new folder, called dynatopTest
 
@@ -14,54 +16,36 @@ dir.create(demo_dir)
 #create a new object, with location of meta data file (which it will make)
 ctch <- dynatopGIS$new(file.path(demo_dir,"meta.json"))
 
-#load up demo files
-dem_file <- system.file("extdata", "SwindaleDTM40m.tif", package="dynatopGIS", mustWork = TRUE)
-channel_file <- system.file("extdata", "SwindaleRiverNetwork.shp", package="dynatopGIS", mustWork = TRUE)
-sp_lines <- raster::shapefile(channel_file)
-head(sp_lines)
-property_names <- c(channel_id="identifier",
-                    endNode="endNode",
-                    startNode="startNode",
-                    length="length")
-
-
 #my actual files
-setwd("C:/Users/John/Documents/HBTopModel/HB/knb-lter-hbr.211.2")
-dem <- raster("dem1m.tif")
+dem <- raster("C:/Users/John/Documents/VT Research/HBTopModel/HB/knb-lter-hbr.211.2/dem1m.tif")
 #plot(dem)
 
 setwd("C:/Users/John/Documents/HBTopModel/HB/hbef_wsheds")
-w3 <- readOGR(dsn='.',layer = 'hbef_wsheds')
-
+w3 <- readOGR(dsn='C:/Users/John/Documents/VT Research/HBTopModel/HB/hbef_wsheds',layer = 'hbef_wsheds')
 w3 <- w3 %>% filter(WS == "WS3")
-plot(w3)
-
-w3_p <- spTransform(w3, crs(dem))
-dem_crop <- crop(dem, w3_p)
-w3_dem <- mask(dem_crop, w3_p)
-raster::writeRaster(w3_dem, "w3_dem.tif", format = "GTiff")
+#plot(w3)
+#clipping DEM by W3 boundary
+#w3_p <- spTransform(w3, crs(dem))
+#dem_crop <- crop(dem, w3_p)
+#w3_dem <- mask(dem_crop, w3_p)
+#raster::writeRaster(w3_dem, "w3_dem.tif", format = "GTiff")
+w3_dem <- raster("C:/Users/John/Documents/VT Research/HBTopModel/w3_dem.tif")
 
 plot(w3_dem, add = TRUE)
-plot(stream_p, add = TRUE)
 #stream shapefile, from Jensen et al. 2017
-setwd("C:/Users/John/Documents/HBTopModel/HB/hbstream/HB_Shapefiles")
-stream <- readOGR("hb42_master.shp")
-s <- raster::shapefile("hb42_master.shp")
+#original stream shapefile
+stream <- readOGR("./HB/hbstream/HB_Shapefiles/hb42_master.shp")
+#streams with start and end nodes
+stream <- readOGR("./HB/hbstream/HB_Shapefiles/hb42_master.shp")
+
 stream_p <- spTransform(stream, crs(dem))
-
-mapview(list(w3_p,stream_p))
-
+#mapview(list(w3_p,stream_p))
 stream_c <- crop(stream_p, w3_p)
-#make a hillshade
-wbt_hillshade(dem = "w3_dem.tif",
-              output = "w3_hillshade.tif",
-              azimuth = 115)
 
-hill <- raster("w3_hillshade.tif")
-  plot(hill, col = "grey")
 
 #adding dem to the project
-ctch$add_dem(dem)
+library(igraph)
+ctch$add_dem(w3_dem)
 ctch$add_channel(stream_c)
 
 ctch$compute_areas()
@@ -105,16 +89,22 @@ out <- merge( merge(swindale_obs,sim1),sim2)
 names(out) <- c(names(swindale_obs),'sim_1','sim_2')
 plot(out[,c('Flow','sim_1','sim_2')], main="Discharge",ylab="m3/s",legend.loc="topright")
 
-#trying to follow the set up guide again
-library(dynatopGIS)
-demo_dir <- tempfile("dygis")
-dir.create(demo_dir)
-#create a new dynatop object
-ctch <- dynatopGIS$new(file.path(demo_dir,"meta.json"))
 
-#add catchment data
-#specify paths to the DEM and stream flowlines
-channel_file <- "C:/Users/John/Documents/VT Research/HBTopModel/HB/hbstream/HB_Shapefiles/hb42_master.shp"
+#the graveyard
+##########################
+#load up demo files
+dem_file <- system.file("extdata", "SwindaleDTM40m.tif", package="dynatopGIS", mustWork = TRUE)
+channel_file <- system.file("extdata", "SwindaleRiverNetwork.shp", package="dynatopGIS", mustWork = TRUE)
+sp_lines <- raster::shapefile(channel_file)
+head(sp_lines)
+property_names <- c(channel_id="identifier",
+                    endNode="endNode",
+                    startNode="startNode",
+                    length="length")
+#make a hillshade
+wbt_hillshade(dem = "w3_dem.tif",
+              output = "w3_hillshade.tif",
+              azimuth = 115)
 
-stream <- readOGR(channel_file)
-
+hill <- raster("w3_hillshade.tif")
+plot(hill, col = "grey")
